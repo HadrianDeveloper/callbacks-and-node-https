b6273ede99a402459031e92749f601a3d3591074
HadrianDeveloper <hadrian.developer@outlook.com>
2023-04-15 15:13:42 +0100

    All functions refactored with helper functions doing all the async work

--- refactor1/index.js
+++ refactor1/index.js
@@ -1,29 +1,40 @@
 
 const { fetchFile, saveAsFile, readLocalFile } = require('./utils');
 
-/*
-//Fetch all text from API and save to md file
+
+
 fetchFile(('confidential'), ({instructions}) => {
     saveAsFile(`${__dirname}/instructions.md`, instructions)
 });
 
-//Fetch all data from API and save filtered data to json file
 fetchFile(('people'), ({people}) => {
     const ncoders = people.filter((u) => u.job.workplace === 'northcoders');
     saveAsFile(`${__dirname}/northcoders.json`, JSON.stringify(ncoders))
 });
-*/
 
 readLocalFile(('northcoders.json'), ({northcoders}) => {
     const usernames = northcoders.map((u) => u.username);
     const interests = [];
-
     usernames.forEach((un) => {
         fetchFile(`people/${un}/interests`, ({person}) => {
             interests.push(person);
             if (interests.length === usernames.length) {
                 saveAsFile(`${__dirname}/interests.json`, JSON.stringify(interests))
             };
-        })
+        });
+    });
+});
+
+readLocalFile(('northcoders.json'), ({northcoders}) => {
+    const usernames = northcoders.map((u) => u.username);
+    const pets = [];
+    let count = 0;
+    usernames.forEach((u) => {
+        fetchFile(`people/${u}/pets`, ({person}) => {
+            if (person) pets.push(person);
+            if (++count === usernames.length) {
+                saveAsFile(`${__dirname}/pets.json`, JSON.stringify(pets));
+            };
+        });
     });
 });
