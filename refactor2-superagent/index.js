5fc9beb76c063b389fc906fba0e7e702f56b4cf0
HadrianDeveloper <hadrian.developer@outlook.com>
2023-04-15 18:11:16 +0100

    superagent alternative applied for http requests

---
+++ refactor2-superagent/index.js
@@ -0,0 +1,39 @@
+
+const { fetchFile, saveAsFile, readLocalFile } = require('./utils');
+
+fetchFile(('confidential'), ({instructions}) => {
+    saveAsFile('instructions.md', instructions)
+});
+
+fetchFile(('people'), ({people}) => {
+    const ncoders = people.filter((u) => u.job.workplace === 'northcoders');
+    saveAsFile('northcoders.json', JSON.stringify(ncoders))
+});
+
+readLocalFile(('northcoders.json'), (list) => {
+    const usernames = list.map((u) => u.username);
+    const interests = [];
+    usernames.forEach((un) => {
+        fetchFile(`people/${un}/interests`, ({person}) => {
+            interests.push(person);
+            if (interests.length === usernames.length) {
+                saveAsFile('interests.json', JSON.stringify(interests))
+            };
+        });
+    });
+});
+
+readLocalFile(('northcoders.json'), (list) => {
+    const usernames = list.map((u) => u.username);
+    const pets = [];
+    let count = 0;
+    usernames.forEach((u) => {
+        fetchFile(`people/${u}/pets`, ({person}) => {
+            count++;
+            if (person) pets.push(person);
+            if (count === usernames.length) {
+                saveAsFile('pets.json', JSON.stringify(pets));
+            };
+        });
+    });
+});
