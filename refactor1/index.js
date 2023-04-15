3d7ca3293af17eb56af86a691a1f690383750e8f
HadrianDeveloper <hadrian.developer@outlook.com>
2023-04-15 14:46:41 +0100

    added readFile helper function and function to fetch data from api using query

--- refactor1/index.js
+++ refactor1/index.js
@@ -1,7 +1,29 @@
 
-const { fetchFile, saveAsFile } = require('./utils');
+const { fetchFile, saveAsFile, readLocalFile } = require('./utils');
 
+/*
+//Fetch all text from API and save to md file
 fetchFile(('confidential'), ({instructions}) => {
-    saveAsFile('instructions.md', instructions)
+    saveAsFile(`${__dirname}/instructions.md`, instructions)
 });
 
+//Fetch all data from API and save filtered data to json file
+fetchFile(('people'), ({people}) => {
+    const ncoders = people.filter((u) => u.job.workplace === 'northcoders');
+    saveAsFile(`${__dirname}/northcoders.json`, JSON.stringify(ncoders))
+});
+*/
+
+readLocalFile(('northcoders.json'), ({northcoders}) => {
+    const usernames = northcoders.map((u) => u.username);
+    const interests = [];
+
+    usernames.forEach((un) => {
+        fetchFile(`people/${un}/interests`, ({person}) => {
+            interests.push(person);
+            if (interests.length === usernames.length) {
+                saveAsFile(`${__dirname}/interests.json`, JSON.stringify(interests))
+            };
+        })
+    });
+});
