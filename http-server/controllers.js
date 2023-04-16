070cb5b435b3834c338d8ed6078fdad8ddac5995
HadrianDeveloper <hadrian.developer@outlook.com>
2023-04-16 09:34:32 +0100

    all fetch from JSON file functions and endpoints complete

--- http-server/controllers.js
+++ http-server/controllers.js
@@ -1,4 +1,5 @@
-const { fetchNorthcoders, fetchIndividual } = require("./models");
+const { fetchNorthcoders, fetchIndividual, fetchSomeonesPets, fetchSomeonesInterests } = require("./models");
+
 
 exports.getNorthcoders = (req, res) => {
     fetchNorthcoders((jsonData) => {
@@ -9,4 +10,35 @@ exports.getNorthcoders = (req, res) => {
     })
 };
 
-exports.getIndividual = (req, res) => {};
+exports.getIndividual = (req, res) => {
+    let {url} = req;
+        url = url.substring(17)
+    fetchIndividual((url), (jsonData) => {
+        res.setHeader('Content-Type', 'application/json');
+        res.statusCode = 200;
+        res.write(jsonData);
+        res.end();
+    })
+};
+
+exports.getSomeonesPets = (req, res) => {
+    let {url} = req;
+        url = url.substring(10)
+    fetchSomeonesPets((url), (jsonData) => {
+        res.setHeader('Content-Type', 'application/json');
+        res.statusCode = 200;
+        res.write(jsonData);
+        res.end();
+    })
+};
+
+exports.getSomeonesInterests = (req, res) => {
+    let {url} = req;
+        url = url.substring(15);
+    fetchSomeonesInterests((url), (jsonData) => {
+        res.setHeader('Content-Type', 'application/json');
+        res.statusCode = 200;
+        res.write(jsonData);
+        res.end()
+    })
+};
--- http-server/models.js
+++ http-server/models.js
@@ -1,4 +1,4 @@
-const {readFile} = require('fs');
+const { readFile } = require('fs');
 
 exports.fetchNorthcoders = (cb) => {
     readFile(`${__dirname}/data/northcoders.json`, 
@@ -7,4 +7,33 @@ exports.fetchNorthcoders = (cb) => {
     })
 };
 
-exports.fetchIndividual = (cb) => {};
+exports.fetchIndividual = (username, cb) => {
+    this.fetchNorthcoders((list) => {
+        const parsedList = JSON.parse(list);
+        const individual = parsedList.northcoders
+            .filter((u) => u.username === username);
+        cb(JSON.stringify(individual))
+    })
+};
+
+exports.fetchSomeonesPets = (username, cb) => {
+    readFile(`${__dirname}/data/pets.json`, 'utf8', (err, res) => {
+        if (err) console.log(err);
+        else {
+            const parsedArray = JSON.parse(res);
+            const target = parsedArray.filter((u) => u.username === username);
+            cb(JSON.stringify(target))
+        }
+    })
+};
+
+exports.fetchSomeonesInterests = (username, cb) => {
+    readFile(`${__dirname}/data/interests.json`, 'utf8', (err, res) => {
+        if (err) console.log(err);
+        else {
+            const parsedArr = JSON.parse(res);
+            const target = parsedArr.filter((u) => u.username === username);
+            cb(JSON.stringify(target));
+        }
+    })
+}
