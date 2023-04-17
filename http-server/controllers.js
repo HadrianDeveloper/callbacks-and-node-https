e1e16450f3edcb378de0bd0d6b7af00d4fcedc77
HadrianDeveloper <hadrian.developer@outlook.com>
2023-04-17 10:04:20 +0100

    All 404s handled

--- http-server/controllers.js
+++ http-server/controllers.js
@@ -1,25 +1,33 @@
 const { fetchNorthcoders, fetchIndividual, fetchSomeonesPets, fetchSomeonesInterests, fetchFilteredPets } = require("./models");
 const { formatResponse, getEndpoint } = require("./utils");
 
+exports.handle404 = (res) => {
+    const errMsg = {msg: `Error! Username not found.`}
+    formatResponse(res, JSON.stringify(errMsg), 404)
+}
+
 exports.getNorthcoders = (req, res) => {
     fetchNorthcoders((jsonData) => formatResponse(res, jsonData))
 };
 
 exports.getIndividual = (req, res) => {
     fetchIndividual((getEndpoint(req.url, 17)), (jsonData) => {
-        formatResponse(res, jsonData)
-    })
+        jsonData.length < 5 ? 
+            this.handle404(res) : formatResponse(res, jsonData) 
+    });
 };
 
 exports.getSomeonesPets = (req, res) => {
     fetchSomeonesPets((getEndpoint(req.url, 10)), (jsonData) => {
-        formatResponse(res, jsonData)
-    })
+        jsonData.length < 5 ? 
+            this.handle404(res) : formatResponse(res, jsonData) 
+    });
 };
 
 exports.getSomeonesInterests = (req, res) => {
     fetchSomeonesInterests((getEndpoint(req.url, 15)), (jsonData) => {
-        formatResponse(res, jsonData)
+        jsonData.length < 5 ? 
+            this.handle404(res) : formatResponse(res, jsonData) 
     })
 };
 
@@ -27,4 +35,4 @@ exports.getFilteredPets = (req, res) => {
     fetchFilteredPets((getEndpoint(req.url, 10)), (jsonData) => {
         formatResponse(res, jsonData)
     });
-}
+};
